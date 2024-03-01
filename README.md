bd94b73b875de84849c44ec604f1184abecc1c3c
zhujinwang <zhujinwang@sunteng.com>
2024-03-01 11:56:18 +0800

    feat: 增加createDict方法

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # monorepo项目
 基于vue3+rollup+vitepress，集成了组件库+方法库+文档（待完善）的monorepo仓库
 
+> 文档地址：https://zjwmmx.github.io
+
 ## 运行
 
 ### 本地实时打包包管理器
--- packages/base/src/dict.utils.js
+++ packages/base/src/dict.utils.js
@@ -1,109 +1,87 @@
-import _ from "lodash";
+import _ from 'lodash'
 
-export function createDict(
-  dataSource,
-  { labelKey, valueKey } = { labelKey: "label", valueKey: "value" }
-) {
-  // dataSource.map(item => ({
-
-  // }))
-
-  const options = dataSource;
-  const values = _.map(options, "value");
-  const labels = _.map(options, "label");
-
-  function getOption(value) {
-    return _.find(options, { value });
-  }
-}
-
-/**
- {
-  Color: 'red'
- }
- */
 function normalizeValue(item, valueKey) {
-  if (_.isString(item)) return item;
-  if (_.isObject(item)) return _.get(item, valueKey);
+  if (_.isString(item)) return item
+  if (_.isObject(item)) return _.get(item, valueKey)
 }
 
 function normalizeLabel(item, labelKey) {
-  if (_.isString(item)) return item;
-  if (_.isObject(item)) return _.get(item, labelKey);
+  if (_.isString(item)) return item
+  if (_.isObject(item)) return _.get(item, labelKey)
 }
 
 export function createDict(
   dataSource,
-  { labelKey, valueKey } = { labelKey: "label", valueKey: "value" }
+  { labelKey, valueKey } = { labelKey: 'label', valueKey: 'value' }
 ) {
-  let isArray = Array.isArray(dataSource);
-  let normalizeData;
+  let isArray = Array.isArray(dataSource)
+  let normalizeData
   if (isArray) {
     normalizeData = _.map(dataSource, (item) => {
       return {
         ...item,
         label: normalizeLabel(item, labelKey),
-        value: normalizeValue(item, valueKey),
-      };
-    });
+        value: normalizeValue(item, valueKey)
+      }
+    })
   } else {
     normalizeData = _.mapValues(dataSource, (item, key) => {
       return {
         ...item,
         label: normalizeLabel(item, labelKey),
-        value: normalizeValue(item, valueKey),
-      };
-    });
+        value: normalizeValue(item, valueKey)
+      }
+    })
   }
 
   const enumData = isArray
     ? normalizeData.reduce((acc, item) => {
         return {
           ...acc,
-          [item.value]: item.value,
-        };
+          [item.value]: item.value
+        }
       }, {})
-    : _.mapValues(normalizeData, "value");
-  const options = Object.values(normalizeData);
-  const values = _.map(options, "value");
-  const labels = _.map(options, "label");
+    : _.mapValues(normalizeData, 'value')
+  const options = Object.values(normalizeData)
+  const values = _.map(options, 'value')
+  const labels = _.map(options, 'label')
   function getOption(value) {
-    return _.find(options, { value });
+    return _.find(options, { value })
   }
   function getField(value, fieldName) {
-    return _.get(getOption(value), fieldName);
+    return _.get(getOption(value), fieldName)
   }
   function getLabel(value) {
-    return getField(value, "label");
+    return getField(value, 'label')
   }
   // 获取部分的选项
   function pickOptions(values = []) {
     return options.filter((item) => {
-      return values.includes(item.value);
-    });
+      return values.includes(item.value)
+    })
   }
   // 排除部分的选项
   function omitOptions(values = []) {
     return options.filter((item) => {
-      return !values.includes(item.value);
-    });
+      return !values.includes(item.value)
+    })
   }
   // 获取部分的值
   function pickValues(valueList = []) {
     return values.filter((item) => {
-      return valueList.includes(item);
-    });
+      return valueList.includes(item)
+    })
   }
   // 排除部分的值
   function omitValues(valueList = []) {
     return values.filter((item) => {
-      return !valueList.includes(item);
-    });
+      return !valueList.includes(item)
+    })
   }
   function invoke(value, propertyPath, ...params) {
-    const option = getOption(value);
+    const option = getOption(value)
     if (option) {
-      return _.invoke(option, propertyPath, ...params);
+      return _.invoke(option, propertyPath, ...params)
     }
   }
   return {
@@ -119,10 +97,10 @@ export function createDict(
     getLabel,
     pickOptions,
     omitOptions,
-    invoke,
-  };
+    invoke
+  }
 }
 
 export function stringArrayToOptions(arr = []) {
-  return arr.map((item) => ({ label: item, value: item }));
+  return arr.map((item) => ({ label: item, value: item }))
 }
